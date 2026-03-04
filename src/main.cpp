42ba85d40f826b659cde9a2b04769937bed78a2c
Aaron B <120134978+Waff1e5@users.noreply.github.com>
2026-03-04 13:28:38 -0500

    milestone 1

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,23 @@
-#include <FEH.h>
 #include <Arduino.h>
-
+#include <FEH.h>
 
 // Declare things like Motors, Servos, etc. here
 // For example:
 // FEHMotor leftMotor(FEHMotor::Motor0, 6.0);
 // FEHServo servo(FEHServo::Servo0);
 
+FEHMotor rightMotor(FEHMotor::Motor0, 7.2);
+FEHMotor leftMotor(FEHMotor::Motor1, 7.2);
 
-void ERCMain()
-{
-    // Your code here!
+void drive(int percent);
 
-    // Or just use the TestGUI function
-    TestGUI();
+void ERCMain() {
+  drive(50);
+  Sleep(2.0);
+  drive(0);
+}
 
+void drive(int percent) {
+  rightMotor.SetPercent(percent);
+  leftMotor.SetPercent(percent);
 }
